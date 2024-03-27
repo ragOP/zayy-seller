569ce3fb8810d5ff1347febfd602497e8496b1cf
ragOP <najmiraghib@gmail.com>
2024-03-28 02:18:57 +0530

    ds

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LoginPage from './pages/LoginPage';
 import DashboardPage from './pages/DashboardPage';
+import AddProduct from './pages/AddProduct'
 
 function App() {
   return (
@@ -11,6 +12,8 @@ function App() {
       <Routes>
         <Route path="/" element={<LoginPage />} />
         <Route path="/dashboard" element={<DashboardPage />} />
+        <Route path="/dashboard/customers" element={<AddProduct />} />
+ 
       </Routes>
     </Router>
   );
